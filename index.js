45884267bd573fdd0b9b5888ba08c0342cd99ee6
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-10-21 01:21:16 +0600

    product data added to the server

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("products");
 
+    app.get("/products", async(req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
